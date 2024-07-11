a10d831e6eebb71002248c1457252a6a0f5f8dec
MateusPitura <mateuspitura@gmail.com>
2024-07-11 19:47:53 -0300

    feat: introduction section

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,28 @@ import PhotoPerfilMe from './assets/images/photo-perfil-me.jpg'
 function App() {
   return (
     <>
-      <div className='flex w-full items-center'>
-        <div className='flex-1 p-5'>
-          <img src={PhotoPerfilMe} className='max-h-[650px] rounded-lg'/>
+      <div className='flex w-full flex-wrap justify-center '>
+        <div className='flex-1 p-2 max-w-[500px] min-w-[300px]'>
+          <img src={PhotoPerfilMe} className='max-w-full rounded-lg' />
         </div>
-        <div className='flex-1 p-5 max-w-[400px]'>
-          <div>OLÁ.</div>
-          <div>Mateus</div>
-          <div>Pitura</div>
-          <div>Eu sou apaixado em resolver problemas e criar soluções usando meu conhecimento em engenharia de software</div>
-          <div>Nos últimos anos, venho desenvolvendo programas para web e para dispositivos móveis, além de atuar na elaboração do backend e na base de dados</div>
+        <div className='flex-1 p-5 justify-start text-left max-w-[500px] min-w-[300px]'>
+          <span className='text-[45px] text-[#FF5449]'>OLÁ.</span>
+          <div className='mb-10 relative right-0 md:right-16'>
+            <div className='mb-2'>
+              <span className='p-2 bg-[#15130B] text-[57px] text-[#E8E2D4]'>MATEUS</span>
+            </div>
+            <div className='mt-2'>
+              <span className='p-2 bg-[#15130B] text-[57px] text-[#E8E2D4]'>PITURA</span>
+            </div>
+          </div>
+          <div>
+            <div className='mb-2'>
+              <span className='text-[#1E1B13]'>Eu sou apaixado em resolver problemas e criar soluções usando meu conhecimento em Engenharia de Software</span>
+            </div>
+            <div className='mt-2'>
+              <span className='text-[#1E1B13]'>Nos últimos anos, venho desenvolvendo programas para web e para dispositivos móveis, além de atuar na elaboração de backend e com banco de dados</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
