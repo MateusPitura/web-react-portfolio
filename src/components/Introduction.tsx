45b69cc6d01082461043771e6181be494b216a26
MateusPitura <mateuspitura@gmail.com>
2024-08-03 11:57:40 -0300

    refactor: improved size of image

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,4 +1,4 @@
-import PhotoPerfilMe from '../assets/images/photo-perfil-me.webp'
+import PhotoPerfilLarge from '../assets/images/photo-perfil-large.webp'
 import { useTranslation } from 'react-i18next'
 
 function Introduction() {
@@ -6,9 +6,9 @@ function Introduction() {
 
     return (
         <section className='flex flex-wrap px-5 pb-20 justify-center w-full'>
-            <div className='flex-1 p-2 max-w-[31.25rem] min-w-[18.75rem]'>
+            <div className='flex-1 p-2 max-w-[31.25rem] sm:min-w-[25rem] min-w-[18.75rem]'>
                 <div className='border-primary-light border-2 max-w-[6.25rem] relative right-5 top-5'></div>
-                <img src={PhotoPerfilMe} className='max-w-full rounded-lg shadow-sm' alt="Perfil picture"/>
+                <img src={PhotoPerfilLarge} className='max-w-full rounded-lg shadow-sm' alt="Perfil picture" width={450} height={600}/>
             </div>
             <div className='flex-1 p-2 justify-start text-left max-w-[31.25rem] min-w-[18.75rem]'>
                 <span className='text-[2.8125rem] text-primary-light'>Full Stack.</span>
