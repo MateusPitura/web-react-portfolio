c2183c2d53537f1ea5496b44202193982ace6eab
MateusPitura <mateuspitura@gmail.com>
2024-08-04 17:09:38 -0300

    feat: improving

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,5 +1,4 @@
 import PhotoPerfilLarge from '../assets/images/photo-perfil-large.webp'
-import PhotoPerfilSmall from '../assets/images/photo-perfil-small.webp'
 import { useTranslation } from 'react-i18next'
 
 function Introduction() {
@@ -10,10 +9,11 @@ function Introduction() {
             <div className='flex-1 p-2 max-w-[31.25rem] sm:min-w-[25rem] min-w-[18.75rem]'>
                 <div className='border-primary-light border-2 max-w-[6.25rem] relative right-5 top-5 z-10'></div>
                 <img
-                    src={window.innerWidth<=412?PhotoPerfilSmall:PhotoPerfilLarge}
+                    src={PhotoPerfilLarge}
                     alt="Perfil picture"
                     className='flex rounded-lg shadow-md'
                     height={600} width={450}
+                    onLoad={()=>{console.log('teste')}}
                 />
             </div>
             <div className='flex-1 p-2 justify-start text-left max-w-[31.25rem] min-w-[18.75rem]'>
