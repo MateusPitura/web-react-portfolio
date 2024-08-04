f9e87a1ba64eea0a63f17d65e208a50b43563b7d
MateusPitura <mateuspitura@gmail.com>
2024-08-04 16:22:36 -0300

    feat: improved performance

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,5 @@
 import SocialGithubLogo from '../assets/images/social-github-small.webp'
 import SocialLinkedinLogo from '../assets/images/social-linkedin-small.webp'
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import 'react-lazy-load-image-component/src/effects/opacity.css';
 
 export default function Contact() {
     return (
@@ -9,17 +7,15 @@ export default function Contact() {
             <div className='flex flex-1 flex-wrap min-w-full items-center justify-center gap-10'>
                 <div className='flex-1 p-2 flex items-center justify-center max-w-[15.625rem] min-w-[10rem]'>
                     <a className=" hover:opacity-50" href="https://github.com/MateusPitura" target='_blank' aria-label="Link to Github">
-                        <LazyLoadImage
-                            src={SocialGithubLogo} alt="Github logo" width={234} height={78}
-                            effect='opacity'
+                        <img
+                            src={SocialGithubLogo} alt="Github logo" width={234} height={78} loading='lazy'
                         />
                     </a>
                 </div>
                 <div className='flex-1 p-2 flex items-center justify-center max-w-[15.625rem] min-w-[10rem]'>
                     <a className=" hover:opacity-50" href="https://www.linkedin.com/in/mateuspitura" target='_blank' aria-label="Link to Linkedin">
-                        <LazyLoadImage
-                            src={SocialLinkedinLogo} alt="Linkedin logo" width={234} height={78}
-                            effect='opacity'
+                        <img
+                            src={SocialLinkedinLogo} alt="Linkedin logo" width={234} height={78} loading='lazy'
                         />
                     </a>
                 </div>
