1d27c2df6be5de5c689524c97e4b26111e13a50b
MateusPitura <mateuspitura@gmail.com>
2024-11-15 12:05:21 -0300

    feat: :sparkles: improved security with CSP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,58 +1,69 @@
-import { AppBar, Divider } from '@mui/material'
-import { Translate, DarkMode, LightMode } from '@mui/icons-material'
-import "../i18n"
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { AppBar, Divider } from "@mui/material";
+import { Translate, DarkMode, LightMode } from "@mui/icons-material";
+import "../i18n";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
-    const { i18n } = useTranslation();
-    const [dark, setDark] = useState(false);
+  const { i18n } = useTranslation();
+  const [dark, setDark] = useState(false);
 
-    const darkModeHandler = () => {
-        setDark(!dark);
-        document.body.classList.toggle("dark");
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
+
+  const toggleLanguage = () => {
+    if (i18n.language === "pt") {
+      i18n.changeLanguage("en");
+      return;
     }
+    i18n.changeLanguage("pt");
+  };
 
-    const toggleLanguage = () => {
-        if (i18n.language === 'pt') {
-            i18n.changeLanguage('en')
-            return
-        }
-        i18n.changeLanguage('pt')
-    }
-
-    return (
-        <AppBar
-            position="sticky"
-            style={{
-                boxShadow: 'none',
-                backgroundColor: 'light-gray',
-                zIndex: 1501,
-            }}
-        >
-            <header className='flex p-2 sm:px-10 items-center min-h-[5rem] space-x-1 bg-surface-light dark:bg-surface-dark'>
-                <nav className='flex-2 flex justify-start items-center'>
-                    <Link to="/" className='hover:opacity-50'>
-                        <span className='text-primary-light text-[2rem] font-customCaveat'>
-                            Mateus Pitura
-                        </span>
-                    </Link>
-                </nav>
-                <div className='flex-1 flex justify-end items-center text-center gap-5'>
-                    <button onClick={toggleLanguage} className='hover:opacity-50' name="Change page language">
-                        <span className='text-onSurface-light dark:text-onSurface-dark font-customUbuntu text-[1rem]'>
-                            {i18n.language == "pt" ? 'PT' : 'EN'}
-                        </span>
-                        <Translate className='text-onSurface-light dark:text-onSurface-dark m-1' />
-                    </button>
-                    <button name="LightMode change" onClick={darkModeHandler} className='hover:opacity-50'>
-                        {!dark && <DarkMode className='text-onSurface-light dark:text-onSurface-dark m-1' />}
-                        {dark && <LightMode className='text-onSurface-light dark:text-onSurface-dark m-1' />}
-                    </button>
-                </div>
-            </header>
-            <Divider className='bg-surface-light dark:bg-outline-light' />
-        </AppBar>
-    )
+  return (
+    <AppBar
+      position="sticky"
+      style={{
+        boxShadow: "none",
+        backgroundColor: "light-gray",
+        zIndex: 1501,
+      }}
+    >
+      <header className="flex p-2 sm:px-10 items-center min-h-[5rem] space-x-1 bg-surface-light dark:bg-surface-dark">
+        <nav className="flex-2 flex justify-start items-center">
+          <Link to="/" className="hover:opacity-50">
+            <span className="text-primary-light text-[2rem] font-customCaveat">
+              Mateus Pitura
+            </span>
+          </Link>
+        </nav>
+        <div className="flex-1 flex justify-end items-center text-center gap-5">
+          <button
+            onClick={toggleLanguage}
+            className="hover:opacity-50"
+            aria-label="Change page language"
+          >
+            <span className="text-onSurface-light dark:text-onSurface-dark font-customUbuntu text-[1rem]">
+              {i18n.language == "pt" ? "PT" : "EN"}
+            </span>
+            <Translate className="text-onSurface-light dark:text-onSurface-dark m-1" />
+          </button>
+          <button
+            aria-label="LightMode change"
+            onClick={darkModeHandler}
+            className="hover:opacity-50"
+          >
+            {dark ? (
+              <LightMode className="text-onSurface-light dark:text-onSurface-dark m-1" />
+            ) : (
+              <DarkMode className="text-onSurface-light dark:text-onSurface-dark m-1" />
+            )}
+          </button>
+        </div>
+      </header>
+      <Divider className="bg-surface-light dark:bg-outline-light" />
+    </AppBar>
+  );
 }
