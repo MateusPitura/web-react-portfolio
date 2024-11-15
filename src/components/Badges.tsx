a87845a24e561b02c282a5ef5005dc2b42df1469
MateusPitura <mateuspitura@gmail.com>
2024-11-15 12:48:55 -0300

    fix: :bug: added fix size to image badges

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,21 +1,26 @@
 import { Badge } from "../types/Badge";
 
 interface BadgesProperties {
-    badges?: Badge[];
+  badges?: Badge[];
 }
 
 function Badges({ badges }: BadgesProperties) {
-    return (
-        <div className="pt-5 flex flex-wrap gap-1 w-full">
-            {badges?.map(item =>
-                <div key={item.id}>
-                    <div className='flex justify-center flex-wrap'>
-                        <img id={item.reference} className='rounded-lg h-[1.75rem]' src={item.imageUrl} alt={`Stack ${item.reference}`} height={28}/>
-                    </div>
-                </div>
-            )}
+  return (
+    <div className="pt-5 flex flex-wrap gap-1 w-full">
+      {badges?.map((item) => (
+        <div key={item.id}>
+          <div className="flex justify-center flex-wrap">
+            <img
+              id={item.reference}
+              className="rounded-lg h-[1.75rem] w-full"
+              src={item.imageUrl}
+              alt={`Stack ${item.reference}`}
+            />
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Badges;
--- src/components/CardStack.tsx
+++ src/components/CardStack.tsx
@@ -1,8 +1,8 @@
-import { Divider } from '@mui/material'
-import { ReactNode, RefObject } from 'react';
-import { Badge } from '../types/Badge';
-import Tooltip from './Tooltip'
-import Slider from 'react-slick';
+import { Divider } from "@mui/material";
+import { ReactNode, RefObject } from "react";
+import { Badge } from "../types/Badge";
+import Tooltip from "./Tooltip";
+import Slider from "react-slick";
 
 interface CardStackProperties {
   title: string | ReactNode;
@@ -16,71 +16,88 @@ const scrollToElement = (id: string) => {
   const element = document.getElementById(id);
   if (element) {
     const offset = window.innerHeight / 2 - element.offsetHeight / 2;
-    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    const elementPosition =
+      element.getBoundingClientRect().top + window.scrollY;
     const offsetPosition = elementPosition - offset;
 
     window.scrollTo({
       top: offsetPosition,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
 
     try {
-      element.classList.remove('blink');
+      element.classList.remove("blink");
     } catch (error) {
       return;
     }
     setTimeout(() => {
-      element.classList.add('blink');
-    }, 1000)
+      element.classList.add("blink");
+    }, 1000);
   }
 };
 
-function CardStack({ title, badges, sliderRef, isTooltipOpen, setIsTooltipOpen }: CardStackProperties) {
-
+function CardStack({
+  title,
+  badges,
+  sliderRef,
+  isTooltipOpen,
+  setIsTooltipOpen,
+}: CardStackProperties) {
   const stacksLastIndex = Number(badges?.length) - 1;
 
   const goToSlide = (index: number) => {
     if (sliderRef.current) {
-      sliderRef.current.slickGoTo(index)
+      sliderRef.current.slickGoTo(index);
     }
-  }
+  };
 
   return (
-    <div className='flex-1 flex-wrap min-w-[12.5rem] p-2 m-2'>
-      <div className='pb-5'>
-        <span className='text-[1.75rem] text-onSurface-light dark:text-onSurface-dark font-customUbuntu'>{title}</span>
+    <div className="flex-1 flex-wrap min-w-[12.5rem] p-2 m-2">
+      <div className="pb-5">
+        <span className="text-[1.75rem] text-onSurface-light dark:text-onSurface-dark font-customUbuntu">
+          {title}
+        </span>
       </div>
-      <div className='space-y-3'>
+      <div className="space-y-3">
         {badges?.map((item, index) => (
           <div key={item.id}>
-            <div className='flex justify-center flex-wrap pb-2'>
+            <div className="flex justify-center flex-wrap pb-2">
               <button
-                className='hover:opacity-50'
+                className="hover:opacity-50"
                 name={`Stack ${item.reference}`}
                 onClick={() => {
-                  setIsTooltipOpen(false)
-                  if (typeof item?.sliderIndex === 'number') {
-                    goToSlide(item.sliderIndex)
+                  setIsTooltipOpen(false);
+                  if (typeof item?.sliderIndex === "number") {
+                    goToSlide(item.sliderIndex);
                   }
-                  scrollToElement(item.reference)
-                }
-                }
+                  scrollToElement(item.reference);
+                }}
               >
-                {index == stacksLastIndex && isTooltipOpen ?
+                {index == stacksLastIndex && isTooltipOpen ? (
                   <Tooltip open={isTooltipOpen}>
-                    <img className='rounded-lg h-[1.75rem]' src={item.imageUrl} alt={`Stack ${item.reference}`} height={28}/>
+                    <img
+                      className="rounded-lg h-[1.75rem] w-full"
+                      src={item.imageUrl}
+                      alt={`Stack ${item.reference}`}
+                    />
                   </Tooltip>
-                  :
-                  <img className='rounded-lg h-[1.75rem]' src={item.imageUrl} alt={`Stack ${item.reference}`} height={28}/>
-                }
+                ) : (
+                  <img
+                    className="rounded-lg h-[1.75rem] w-full"
+                    src={item.imageUrl}
+                    alt={`Stack ${item.reference}`}
+                  />
+                )}
               </button>
             </div>
-            {index != stacksLastIndex && <Divider className='bg-surface-light dark:bg-outline-light' />}
+            {index != stacksLastIndex && (
+              <Divider className="bg-surface-light dark:bg-outline-light" />
+            )}
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default CardStack
+export default CardStack;
