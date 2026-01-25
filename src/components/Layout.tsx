f78d616a108855c53589e084403e1d317f0cf419
MateusPitura <mateuspitura@gmail.com>
2026-01-25 20:34:31 +0000

    feat: add useLocation to Layout component for dynamic ErrorBoundary key

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense, type ReactNode } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Loading from "../pages/Loading";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -7,8 +7,10 @@ import Header from "./Header";
 const ErrorBoundary = lazy(() => import("../pages/ErrorBoundary"));
 
 export function Layout(): ReactNode {
+  const { pathname } = useLocation()
+
   return (
-    <ErrorBoundary>
+    <ErrorBoundary key={pathname}>
       <div className="h-screen flex flex-col">
         <Header />
         <Suspense fallback={<Loading />}>
