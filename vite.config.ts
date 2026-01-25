3384a66c293a31bd838dfd603e8a6f5dd6495726
MateusPitura <mateuspitura@gmail.com>
2026-01-25 20:43:07 +0000

    feat: update Vite configuration to include versioning in asset file names

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { version } from './package.json'
 
 export default ({ mode }: { mode: string }) => {
   return defineConfig({
     plugins: [react()],
     base: mode === "production" ? "/web-react-portfolio" : "/",
+    build: {
+      // assetsDir: "static"
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            // if (
+            //   /\.(png|jpe?g|svg|gif|webp|ico)$/i.test(assetInfo.name ?? "")
+            // ) {
+            //   return `assets/[name][extname]`;
+            // }
+
+            return `${version}/[name]-[hash][extname]`;
+          },
+        },
+      },
+    },
   });
 };
