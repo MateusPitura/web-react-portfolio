5442423bcd8a636008fd2d5f5fdeab421d9917ac
MateusPitura <mateuspitura@gmail.com>
2026-01-29 00:10:46 +0000

    feat: enhance Badges component with loading state and improved image rendering

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import { Badge } from "../types/Badge";
+import classNames from "classnames";
 
 interface BadgesProperties {
   badge: Badge;
 }
 
 function Badges({ badge }: BadgesProperties) {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <div className="flex justify-center items-center">
+    <div className={classNames("relative h-[1.75rem]", loaded || "w-20")}>
+      {!loaded && (
+        <div className="absolute inset-0 bg-neutral-200 dark:bg-neutral-600 animate-pulse rounded-md" />
+      )}
       <img
         id={badge.reference}
-        className="rounded-lg h-[1.75rem] w-full"
         src={badge.imageUrl}
         alt={`Stack ${badge.reference}`}
+        className={classNames(
+          "rounded-md shadow-md w-full h-full object-cover transition-opacity duration-500",
+          {
+            "opacity-0": !loaded,
+            "opacity-100": loaded,
+          },
+        )}
+        loading="lazy"
+        onLoad={() => setLoaded(true)}
       />
     </div>
   );
